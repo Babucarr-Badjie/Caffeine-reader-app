d9fe3b3a9241e476d698a0122b1907db6217f7c2
babucarr <babucarr.badjie12@gmail.com>
2025-04-08 21:50:29 +0800

    history component updated

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,3 +1,26 @@
+import { coffeeConsumptionHistory } from "../utils";
+
 export default function History() {
-    return <></>;
-  }
+  return (
+    <>
+      <div className="section-header">
+        <i className="fa-solid fa-timeline" />
+        <h2>History</h2>
+      </div>
+      <p>
+        <i>Hover for more information</i>
+      </p>
+      <div className="coffee-history">
+        {Object.keys(coffeeConsumptionHistory)
+          .sort((a, b) => b - a)
+          .map((utcTime, coffeeIndex) => {
+            return (
+              <div key={coffeeIndex}>
+                <i className="fa-solid fa-mug-hot"></i>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
